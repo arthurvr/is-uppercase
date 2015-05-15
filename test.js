e3abb0e7f4a17b08abf435f9f334c6bfe165f02f
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-05-15 21:55:13 +0200

    init

---
+++ test.js
@@ -0,0 +1,20 @@
+'use strict';
+var assert = require('assert');
+var isUppercase = require('./');
+
+it('should test if a string is all uppercase', function () {
+	assert(isUppercase('UNICORNS'));
+	assert(!isUppercase('uNICORNS'));
+	assert(isUppercase('1'));
+	assert(isUppercase('A'));
+	assert(!isUppercase('a'));
+	assert(isUppercase('💩'));
+});
+
+it('should throw when passing non-string input', function () {
+	[function () {}, null, undefined, 23].forEach(function (input) {
+		assert.throws(function () {
+			isLowercase(input);
+		});
+	});
+});
